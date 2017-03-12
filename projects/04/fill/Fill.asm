117bb5a5baabc844de3ecb4e42705b08de7e3ec1
Natalie Arellano <natalie.p.arellano@gmail.com>
2017-03-12 13:56:24 -0400

    Finish project 4, fill screen

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,51 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+// Setup
+@SCREEN
+D=A
+@addr // Initialize addr
+M=D
+@8191
+D=A
+@addr // Set addr
+M=D+M
+
+@color
+M=0 // Default is white
+
+@KBD
+D=M
+
+@SET_BLACK
+D;JNE
+
+(SET_SCREEN)
+@SCREEN
+D=A
+
+@addr
+D=M-D
+
+@LOOP
+D;JLT // Start over (read keyboard) if we've done the whole screen
+
+@color
+D=M
+
+@addr
+A=M // Move to the address stored in @addr
+M=D // Set color
+
+@addr
+M=M-1
+
+@SET_SCREEN
+0;JMP
+
+(SET_BLACK)
+@color
+M=-1
+@SET_SCREEN
+0;JMP
