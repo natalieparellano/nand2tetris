b44fe06128b7d8443356caf079fd132499f88173
Natalie Arellano <natalie.p.arellano@gmail.com>
2017-03-11 20:16:25 -0500

    Finish project 4, multiplication

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,41 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Setup
+@product
+M=0
+
+@R0
+D=M
+
+@i
+M=D
+
+(LOOP)
+@i
+D=M // Load value of i
+M=M-1 // Decrement stored value of i
+
+@SAVE
+D;JEQ // Terminate loop based on previous stored value of i
+
+@R1
+D=M
+
+@product // Increment product
+M=D+M
+
+@LOOP
+0;JMP
+
+(SAVE)
+@product
+D=M
+
+@R2
+M=D
+
+(DONE)
+@DONE
+0;JMP
